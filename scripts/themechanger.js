55badf1e5f5b01e59a38510ebe443ea67839e8b8
ievgenii <miagkovi@gmail.com>
2017-01-14 19:26:40 +0200

    ok now

--- scripts/themechanger.js
+++
@@ -1,7 +0,0 @@
- $("document").ready(function() {
- 	var themeChanger = "<p>&#x07DB;</p>";
- 	
- 	$("body").append(themeChanger);
- 	themeChanger.addClass("theme-changer")
- 	$(".theme-changer").css({"position": "absolute", "top": "10px", "right": "10px"});
-  });
